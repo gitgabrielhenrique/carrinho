cd44b4dba1e154913dc67de7fad432057e3aefd6
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-08-17 08:54:11 -0300

    update

--- App.tsx
+++ App.tsx
@@ -7,7 +7,8 @@ export default function App() {
   return (
  <SafeAreaView>
   <StatusBar  />
-    <Carrinho></Carrinho>
+  <Carrinho/>
+
 
  </SafeAreaView>
 
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { Dimensions } from "react-native";
 import{Text,Image,StyleSheet} from 'react-native'
 import topo from'../../../assets/topo.png'
 
 
+const width = Dimensions.get('screen').width
+
+
 export  default function Carrinho(){
-return <Image source={topo} style={estilos.topo}></Image>
+return<>
+
+<Image source={topo} style={estilos.topo}></Image>
+<Text style={estilos.titulo}>Detalhes do carrinho</Text>
+</>
 }
 
 const estilos = StyleSheet.create({
 topo:{
-  width: "100%"
-}
+  width: "100%",
+  height:578/768*width,
+},
+  titulo:{
+    width:"100%",
+    position: "absolute",
+    color:"White",
+    textAlign:"center",
+    fontSize:24,
+    lineHeight:26,
+    fontWeight: "bold",
+    padding:16
+  }
 })
 
