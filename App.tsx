254bbeb6645ed910e953aafe3112f0926fbd9501
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-08-23 15:35:18 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
 
+
+import { useFonts,Montserrat_400Regular,
+  Montserrat_400Regular_Italic,
+  Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import React from 'react';
 import { StatusBar,SafeAreaView } from 'react-native';
 import Carrinho from './src/pages/Carrinho';
 
 export default function App() {
+
+  const [fonteCarregada]=useFonts({
+"Montregular": Montserrat_400Regular,
+"Montbold":Montserrat_700Bold,
+"Montitalico":Montserrat_400Regular_Italic
+
+  })
+if(!fonteCarregada){
+  return null;
+}
+
   return (
  <SafeAreaView>
   <StatusBar  />
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, View } from "react-native";
 import{Text,Image,StyleSheet} from 'react-native'
 import topo from'../../../assets/topo.png'
 
@@ -12,6 +12,10 @@ return<>
 
 <Image source={topo} style={estilos.topo}></Image>
 <Text style={estilos.titulo}>Detalhes do carrinho</Text>
+<View style={estilos.carrinho}>
+<Text style={estilos.nome}>carrinho de compras</Text>
+</View>
+
 </>
 }
 
@@ -27,8 +31,17 @@ topo:{
     textAlign:"center",
     fontSize:24,
     lineHeight:26,
-    fontWeight: "bold",
-    padding:16
+    padding:16,
+    fontFamily:"Montitalico"
+  },
+  carrinho:{
+paddingVertical:8,
+paddingHorizontal:16
+  },
+  nome:{
+    fontSize:26,
+    lineHeight:42,
+    color:"#464646"
   }
 })
 
